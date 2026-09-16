42825d492f87a4b4f378fd206db0666a9005e951
xewvvi <xewvvi@gmail.com>
2026-09-16 09:42:22 +0500

    arm64: Restore support for CMDLINE_EXTEND
    This partially reverts commit cae118b6acc3 ("arm64: Drop support for
    CMDLINE_EXTEND"), which removed CONFIG_CMDLINE_EXTEND due to a mismatch
    in parsing order between idreg-override.c and drivers/of/fdt.c.
    
    That mismatch is not fixed here..

--- arch/arm64/kernel/pi/idreg-override.c
+++ arch/arm64/kernel/pi/idreg-override.c
@@ -380,12 +380,13 @@ static __init const u8 *get_bootargs_cmdline(const void *fdt, int node)
 	return strlen(prop) ? prop : NULL;
 }
 
-static __init void parse_cmdline(const void *fdt, int chosen)
-{
+static __init void parse_cmdline(const void *fdt, int chosen) {
 	static char const cmdline[] __initconst = CONFIG_CMDLINE;
 	const u8 *prop = get_bootargs_cmdline(fdt, chosen);
 
-	if (IS_ENABLED(CONFIG_CMDLINE_FORCE) || !prop)
+	if (IS_ENABLED(CONFIG_CMDLINE_EXTEND) ||
+		IS_ENABLED(CONFIG_CMDLINE_FORCE) ||
+		!prop)
 		__parse_cmdline(cmdline, true);
 
 	if (!IS_ENABLED(CONFIG_CMDLINE_FORCE) && prop)
